43fe12757d627bdf12ba2fabe5ea5f10a8aa897f
Aaren Tebbutt <aaren.tebbutt@noshjournal.com>
2017-09-29 13:31:48 +1000

    Replaced target with a Cocoa Touch Framework target

---
+++ Down/Down.h
@@ -0,0 +1,19 @@
+//
+//  Down.h
+//  Down
+//
+//  Created by Aaren Tebbutt on 29/9/17.
+//  Copyright © 2017 Glazed Donut, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Down.
+FOUNDATION_EXPORT double DownVersionNumber;
+
+//! Project version string for Down.
+FOUNDATION_EXPORT const unsigned char DownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Down/PublicHeader.h>
+
+
